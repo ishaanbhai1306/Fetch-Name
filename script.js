e1538b8d91f71a4bac9c5d77ecaa915b8e1831af
ishaanbhai1306 <goelishaan06@gmail.com>
2026-04-01 22:47:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
+const container = document.getElementById("teams-container");
+const loading = document.getElementById("loading");
 
+loading.innerText = "Loading...";
+
+fetch("https://v3.football.api-sports.io/teams?league=39&season=2023", {
+  headers: {
+    "x-apisports-key": "YOUR_API_KEY"
+  }
+})
+.then(res => res.json())
+.then(data => {
+  loading.innerText = "";
+
+  data.response.forEach(teamData => {
+    const team = teamData.team;
+
+    const div = document.createElement("div");
+
+    div.innerHTML =
+      "<img src='" + team.logo + "' width='80'>" +
+      "<h3>" + team.name + "</h3>" +
+      "<p>" + team.country + "</p>";
+
+    container.appendChild(div);
+  });
+});
